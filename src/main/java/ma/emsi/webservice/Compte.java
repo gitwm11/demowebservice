b2bb4f270d5805ba4cf52e41dcf88a2bbfba434b
ElMehdi WADDAD <wadadmehdi11@gmail.com>
2024-05-07 00:11:40 +0100

    commit

--- src/main/java/ma/emsi/webservice/Compte.java
+++ src/main/java/ma/emsi/webservice/Compte.java
@@ -2,46 +2,26 @@ package ma.emsi.webservice;
 
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
-@ XmlAccessorType(XmlAccessType.FIELD)
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@XmlRootElement(name = "compte")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Compte {
     private Integer code ;
     private double solde;
     private Date dateCreation;
 
-    public Compte(Integer code, double v, Date date) {
-        this.code = code;
-        this.solde = v;
-        this.dateCreation = date;
-    }
 
-
-    public Compte() {
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public double getSolde() {
-        return solde;
-    }
-
-    public void setSolde(double solde) {
-        this.solde = solde;
-    }
-
-    public Date getDateCreation() {
-        return dateCreation;
-    }
-
-    public void setDateCreation(Date dateCreation) {
-        this.dateCreation = dateCreation;
-    }
 }
